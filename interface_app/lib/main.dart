10f66189329436d1fe70ad06d726553f87dd449b
Lucas Wang <lucaswzx909@gmail.com>
2021-05-08 15:06:49 -0700

    After class commit for May 8th

--- interface_app/lib/main.dart
+++ interface_app/lib/main.dart
@@ -6,7 +6,29 @@ void main() {
 }
 
 class InterfaceApp extends StatelessWidget {
-  final int _todayNum = 2;
+  final List<String> _daysOfWeek = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+
+  final int _todayIndex = 5;
+
+  final List<String> _things = [
+    "This is a second text in the same column!",
+    "This is the third text in the same column!",
+    "Hey I just called my friend",
+    "Hey I'm like adding way too many text",
+    "Hey this is yet another text",
+    "Do this literALLY EVERY SINGLE TIME",
+    "This is yet another text",
+    "More text!!!!!!",
+    "So much more text",
+  ];
 
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,26 +39,46 @@ class InterfaceApp extends StatelessWidget {
           title: Text("Today is an interesting Day!"),
         ),
         body: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            children: [
-              Text(
-                "Today's number is: $_todayNum",
-                style: TextStyle(
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.indigoAccent,
+          padding: const EdgeInsets.all(20.0),
+          child: Container(
+            width: double.infinity,
+            child: Column(
+              children: [
+                Text(
+                  "Today is: ${_daysOfWeek[_todayIndex]}",
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.indigoAccent,
+                  ),
                 ),
-              ),
-              Text("This is a second text in the same column!"),
-              SizedBox(height: 30),
-              Text("This is the third text in the same column!"),
-              Icon(
-                Icons.add_call,
-                size: 50,
-                color: Colors.pink,
-              ),
-            ],
+                Icon(
+                  Icons.account_balance_wallet,
+                  size: 50,
+                  color: Colors.pink,
+                ),
+                ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: _things.length,
+                  padding: const EdgeInsets.all(5.0),
+                  itemBuilder: (BuildContext context, int index) {
+                    return Container(
+                      height: 50,
+                      child: Row(
+                        children: [
+                          Text("${_things[index]}"),
+                          Icon(
+                            Icons.add_call,
+                            size: 20,
+                            color: Colors.amber,
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                )
+              ],
+            ),
           ),
         ),
       ),
